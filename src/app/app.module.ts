8270bf132f876bb77f46218be1edc9b4b7818a4a
poitras <nicopoitras@gmail.com>
2017-05-11 20:49:21 -0400

    dynamic hero list added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- form module import here
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
